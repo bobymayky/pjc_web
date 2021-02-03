a308644f2a17df240a9c46a9068398078ca0e5a4
Boby Mayky <bobymaiky10@gmail.com>
2021-02-02 21:40:58 -0400

    adicionando rota home.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home/home.component';
 import { LoginComponent } from './login/login/login.component';
 
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'login'},
   { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
 ];
 
 @NgModule({
